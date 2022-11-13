1e2842cb7a3557daa3bd85b927d299fb651f3716
Alex <loalantref@hmail.com>
2022-11-13 18:00:27 +1200

    Начало

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+       byte crocodile = 88;
+        short amu = 15246;
+        int mosquitos = 5000000;
+        long molecules = 150000000000L;
+        float quantity = 5.25324f;
+        double quantity2 = 3.142;
+        System.out.println("Значение переменной crocodile с типом byte равно " + crocodile);
+        System.out.println("Значение переменной amu с типом short равно " + amu);
+        System.out.println("Значение переменной mosquitos с типом int равно " + mosquitos);
+        System.out.println("Значение переменной molecules с типом long равно " + molecules);
+        System.out.println("Значение переменной quantity с типом float равно " + quantity);
+        System.out.println("Значение переменной quantity2 с типом double равно " + quantity2);
+        System.out.println("____________________________________________________________________________________");
+
+
+
+    }
+}
